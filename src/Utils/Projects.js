dc60204bd6fb256dfc87f84fec24b323c5e2c995
Abhisingh755 <singh80020@gmail.com>
2022-01-13 21:43:43 +0530

    update

---
+++ src/Utils/Projects.js
@@ -0,0 +1,29 @@
+export const projects = [
+	{
+		img: 'https://github.com/Abhisingh755/My-portfolio/blob/main/src/assets/lakme.png?raw=true',
+		title: 'Lakme Clone',
+		des: 'Lakmé is an Indian cosmetics brand which is owned by Hindustan Unilever.Our team of 5 contributed our best effort in this project. My role in this team was to handle  Front-End part of the project.',
+		demo: '',
+		live: 'http://lakme-clone.vercel.app/',
+		gitHub: 'https://github.com/nkil123/lakme_clone',
+		technologies: [ 'HTML', 'CSS', 'javaScript'],
+	},
+	{
+		img: "https://github.com/Abhisingh755/My-portfolio/blob/main/src/assets/HealthKart%20-%20India's%20%231%20Authentic%20Online%20Supplement%20Store%20-%20Google%20Chrome%2014-Dec-21%202_53_43%20PM%20(2).png?raw=true",
+		title: 'HealthKart Clone',
+		des: 'HealthKart.com is India’s largest online health & fitness store for men and women.We worked in validation, Authentication & Google Authentication. My role in this team was to handle Front-End part of the project.',
+		demo: '',
+		live: 'https://hemantjayas.github.io/HealthKart/',
+		gitHub: 'https://github.com/Dhruvkagrawal/HealtKart.com-Clone',
+		technologies: ['ExpressJs', 'MongoDB', 'HTML', 'CSS', 'javaScript'],
+	},
+	{
+		img: 'https://github.com/Abhisingh755/My-portfolio/blob/main/src/assets/translate.png?raw=true',
+		title: 'Translate app',
+		des: 'An application to translate the input text in our preferred language.I used HTML, CSS, JavaScript.',
+		demo: '',
+		live: 'https://him-todo.netlify.app/',
+		gitHub: 'https://github.com/Abhisingh755/translate-app',
+		technologies: [  'HTML', 'CSS', 'javaScript'],
+	},
+];
